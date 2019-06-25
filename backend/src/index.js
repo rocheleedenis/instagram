0d5b397374986ab037c902952f3301e45eeedc44
Rochele Edenís <rochele.edenis@gmail.com>
2019-06-25 00:52:34 -0300

    Cria páginas de Feed e New e inicia criação do componente Header.

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const cors     = require('cors');
 
 const app = express();
 
-const server = require('http'.Server(app));
+const server = require('http').Server(app);
 const io     = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://rochele:TsFQTy5Wj44nz9Na@cluster0-pae2m.mongodb.net/test?retryWrites=true&w=majority', {
